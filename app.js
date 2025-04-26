ec7c89fb3e42dc936f8217e818ed14a667b0aa44
FETTAH234 <mitchelleden2@gmail.com>
2025-04-26 16:04:55 +0200

    fiD

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const session = require('express-session');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Connexion à MongoDB
 mongoose.connect(process.env.mongoConnection, {
